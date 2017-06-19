6bc9c41a21c1daf9dbe0e6194a0bf2fb7a365cc7
himanushi <himanushi@users.noreply.github.com>
2017-06-19 23:29:48 +0900

    googleはてブポケット追加

--- sns_button.js
+++ sns_button.js
@@ -17,12 +17,38 @@
 
   _set_facebook = function (selector, title, url) {
     var
-      u = encodeURIComponent(url),
-      href = 'http://www.facebook.com/share.php?title=' + title + '&u=' + u;
+      encode_url = encodeURIComponent(url),
+      href = 'https://www.facebook.com/share.php?title=' + title + '&u=' + encode_url;
 
     selector.append(_a_tag_generator(title, href));
   };
 
-  _set_twitter(d.getElementById('sns_button_twitter'), 'タイトル', 'https://google.com');
-  _set_facebook(d.getElementById('sns_button_facebook'), 'タイトル', 'https://google.com');
+  _set_google = function (selector, title, url) {
+    var
+      href = 'https://plus.google.com/share?url=' + url;
+
+    selector.append(_a_tag_generator(title, href));
+  };
+
+  _set_hatena = function (selector, title, url) {
+    var
+      encode_title = encodeURIComponent(title),
+      href = 'https://b.hatena.ne.jp/add?title=' + encode_title + '&url=' + url;
+
+    selector.append(_a_tag_generator(title, href));
+  };
+
+  _set_pocket = function (selector, title, url) {
+    var
+      encode_url = encodeURIComponent(url),
+      href = 'http://getpocket.com/edit?title=' + title + '&url=' + encode_url;
+
+    selector.append(_a_tag_generator(title, href));
+  };
+
+  _set_twitter(d.getElementById('sns_button_twitter'), 'ツイッター', 'https://google.com');
+  _set_facebook(d.getElementById('sns_button_facebook'), '顔本', 'https://google.com');
+  _set_google(d.getElementById('sns_button_google'), 'Google', 'https://himakan.net');
+  _set_hatena(d.getElementById('sns_button_hatena'), 'はてブ', 'https://google.com');
+  _set_pocket(d.getElementById('sns_button_pocket'), 'ポケット', 'https://google.com');
 })(document);

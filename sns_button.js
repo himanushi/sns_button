6375815b817c3383188d591b98c37a052a361f7c
himanushi <himanushi@users.noreply.github.com>
2017-06-20 00:12:03 +0900

    add line slack mixi

--- sns_button.js
+++ sns_button.js
@@ -46,9 +46,38 @@
     selector.append(_a_tag_generator(title, href));
   };
 
-  _set_twitter(d.getElementById('sns_button_twitter'), 'ツイッター', 'https://google.com');
-  _set_facebook(d.getElementById('sns_button_facebook'), '顔本', 'https://google.com');
-  _set_google(d.getElementById('sns_button_google'), 'Google', 'https://himakan.net');
-  _set_hatena(d.getElementById('sns_button_hatena'), 'はてブ', 'https://google.com');
-  _set_pocket(d.getElementById('sns_button_pocket'), 'ポケット', 'https://google.com');
+  _set_line = function (selector, title, url) {
+    var
+      href = 'https://line.me/R/msg/text/?' + url;
+
+    selector.append(_a_tag_generator(title, href));
+  };
+
+  _set_slack = function (selector, title, url) {
+    var
+      encode_url = encodeURIComponent(url),
+      href = 'http://slackbutton.herokuapp.com/post/new/?url=' + encode_url;
+
+    selector.append(_a_tag_generator(title, href));
+  };
+
+  _set_mixi = function (selector, title, url) {
+    var
+      href = 'http://mixi.jp/recent_voice.pl?status=' + title + '%20' + url;
+
+    selector.append(_a_tag_generator(title, href));
+  };
+
+  _init = function () {
+    _set_twitter(d.getElementById('sns_button_twitter'), 'ツイッター', 'https://google.com');
+    _set_facebook(d.getElementById('sns_button_facebook'), '顔本', 'https://google.com');
+    _set_google(d.getElementById('sns_button_google'), 'Google', 'https://himakan.net');
+    _set_hatena(d.getElementById('sns_button_hatena'), 'はてブ', 'https://google.com');
+    _set_pocket(d.getElementById('sns_button_pocket'), 'ポケット', 'https://google.com');
+    _set_line(d.getElementById('sns_button_line'), 'Line', 'https://google.com');
+    _set_slack(d.getElementById('sns_button_slack'), 'slack', 'https://google.com');
+    _set_mixi(d.getElementById('sns_button_mixi'), 'mixi', 'https://google.com');
+  };
+
+  _init();
 })(document);
